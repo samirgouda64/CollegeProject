20011e8b47fb932de120203c41f8490d47ed4626
Samirgouda64 <samirgouda439@gmail.com>
2024-09-25 09:14:46 +0530

    Add loan link

--- Loan/script.js
+++ Loan/script.js
@@ -1,14 +1,14 @@
-let indexValue = 0;
-function slideShow (){
+// let indexValue = 0;
+// function slideShow (){
 
-    setTimeout(slideShow,2500);
+//     setTimeout(slideShow,2500);
     
-    const img =document.querySelectorAll("img");
-    for(let i = 0; i<img.length; i++){
-        img[i].style.display = "none";
-    }
-    indexValue++;
-    if(indexValue > img.length){indexValue = 1}
-    img[indexValue -1].style.display = "block";
-}
-slideShow();
+//     const img =document.querySelectorAll("img");
+//     for(let i = 0; i<img.length; i++){
+//         img[i].style.display = "none";
+//     }
+//     indexValue++;
+//     if(indexValue > img.length){indexValue = 1}
+//     img[indexValue -1].style.display = "block";
+// }
+// slideShow();
